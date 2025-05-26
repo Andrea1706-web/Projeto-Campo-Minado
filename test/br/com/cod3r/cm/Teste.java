2aa6d806818ffed6e64b43ccb5cba9c8793bf2a5
Andrea Munhoz <deiacris.munhoz@gmail.com>
2025-05-26 14:47:13 -0300

    Adicionar Campo Minado

---
+++ test/br/com/cod3r/cm/Teste.java
@@ -0,0 +1,22 @@
+package br.com.cod3r.cm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void testarSeIgualATres() {
+		int a = 2 + 1;
+		assertEquals(3, a);
+	}
+	@Test
+	void testarSeIgualACinco() {
+		int x = 2 + 10 - 7;
+		assertEquals(5, x);
+				
+	//	fail("Not yet implemented");
+	}
+
+}
